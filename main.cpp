ad29bdd3db804a3941c17c2cba4051c920a5a67b
Oleki <olekignaczak78@gmail.com>
2026-03-01 23:17:53 +0100

    z order sorting working

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <limits>
 #include <array>
+#include <bitset>
 #include <random>
 #include <utility>
 
@@ -65,6 +66,11 @@ uint64_t expand(unsigned int v) {
     return x;
 }
 
+
+//x = x2 x1 x0
+//y = y2 y1 y0
+//z = z2 z1 z0
+//res: z2 y2 x2 z1 y1 x1 z0 y0 x0
 uint64_t getMortonCodeFrom3D(float x, float y, float z, const std::array<std::pair<float,float>,3>& bounds) {
     uint32_t xs = scale(x, bounds[0].first, bounds[0].second);
     uint32_t ys = scale(y, bounds[1].first, bounds[1].second);
@@ -74,7 +80,18 @@ uint64_t getMortonCodeFrom3D(float x, float y, float z, const std::array<std::pa
     uint64_t yy = expand(ys);
     uint64_t zz = expand(zs);
 
-    // interlace (x,y,z) bits in pattern: x₀y₀z₀x₁y₁z₁ [...]
+    // interlace (x,y,z) bits in pattern: [...]x₁y₁z₁x₀y₀z₀. This value determines tree's octant. Octant = (z,y,x)
+
+    // Division values for 8 octants at the same level:
+    // 000 - back-bottom-left
+    // 001 - back-bottom-right
+    // 010 - back-top-left
+    // 011 - back-top-right
+    // 100 - front-bottom-left
+    // 101 - front-bottom-right
+    // 110 - front-top-left
+    // 111 - front-top-right
+
     return xx | (yy << 1) | (zz << 2);
 }
 
@@ -157,27 +174,103 @@ std::vector<Particle> generateParticles(size_t n)
 int main() {
     std::cout << "test11\n";
 
-    // std::vector<Particle> particles = {
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 0.2f, -400.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {300.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 222.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 222.5f, 0.7f}, {0.4f, 0.2f, -8880.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {0.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    //     {500.1f, 0.5f, 0.7f}, {0.4f, 0.2f, 0.9f},{0.8f, 0.3f, 0.1f}, {0.5, 0.2, -0.1},
-    // };
+    // Packets for tree are distributed as:
 
-    size_t n = 1000;
-    std::vector<Particle> particles = generateParticles(n);
+    // Bounding box: [-1000, 1000] along each axis
+    // Level 1 (root octant, first Morton code “package”):
+    //   - [-1000, 0)   // negative half
+    //   - [0, 1000]    // positive half
+
+    // Level 2 (second Morton code package, divide each half of level 1 in half):
+    //   - [-1000, -500)
+    //   - [-500, 0)
+    //   - [0, 500)
+    //   - [500, 1000]
+
+    // Level 3 (third Morton code package, divide each previous quarter in half):
+    //   - [-1000, -750)
+    //   - [-750, -500)
+    //   - [-500, -250)
+    //   - [-250, 0)
+    //   - [0, 250)
+    //   - [250, 500)
+    //   - [500, 750)
+    //   - [750, 1000]
+
+    // Level 4 (fourth package, node size = 2000 / 16 = 125):
+    //   - [-1000, -875), [-875, -750), [-750, -625), ..., [875, 1000]
+
+    // Level 5 (fifth package, node size = 62.5):
+    //   - Each previous interval is halved again
+    //   - Example: [-1000, -937.5), [-937.5, -875), [-875, -812.5), ...
+
+    // ...
+
+    // Level 21 (last package, node size = 2000 / 2^21 ~ 0.00095):
+    //   - Each Morton code package corresponds to a single leaf node
+
+    std::vector<Particle> particles = {
+        // bounds
+        {-1000, -1000, -1000},
+        {-1000, -1000, 1000},
+        {-1000, 1000, -1000},
+        {-1000, 1000, 1000},
+        {1000, -1000, 1000},
+        {1000, -1000, -1000},
+        {1000, 1000, -1000},
+        {1000, 1000, 1000},
+
+        // 000 octant 0: x ∈ [-1000, 0) y ∈ [-1000, 0) z ∈ [-1000, 0)
+        {-50.f, -760.f, -50.f},
+        // {-300.f, -300.f, -300.f},
+        // {-550.f, -550.f, -550.f},
+        // {-800.f, -800.f, -800.f},
+
+        // // 001 octant 1: x ∈ [0, 1000] y ∈ [-1000, 0) z ∈ [-1000, 0)
+        // {50.f, -50.f, -50.f},
+        // {300.f, -300.f, -300.f},
+        // {550.f, -550.f, -550.f},
+        // {800.f, -800.f, -800.f},
+        //
+        // // 010 octant 2: x ∈ [-1000, 0) y ∈ [0, 1000] z ∈ [-1000, 0)
+        // {-50.f, 50.f, -50.f},
+        // {-300.f, 300.f, -300.f},
+        // {-550.f, 550.f, -550.f},
+        // {-800.f, 800.f, -800.f},
+        //
+        // // 011 octant 3 x ∈ [0, 1000] y ∈ [0, 1000] z ∈ [-1000, 0)
+        // {50.f, 50.f, -50.f},
+        // {300.f, 300.f, -300.f},
+        // {550.f, 550.f, -550.f},
+        // {800.f, 800.f, -800.f},
+        //
+        // // 100 octant 4 x ∈ [-1000, 0) y ∈ [-1000, 0) z ∈ [0, 1000]
+        // {-50.f, -50.f, 50.f},
+        // {-300.f, -300.f, 300.f},
+        // {-550.f, -550.f, 550.f},
+        // {-800.f, -800.f, 800.f},
+        //
+        // // 101 octant 5 x ∈ [0, 1000] y ∈ [-1000, 0) z ∈ [0, 1000]
+        // {50.f, -50.f, 50.f},
+        // {300.f, -300.f, 300.f},
+        // {550.f, -550.f, 550.f},
+        // {800.f, -800.f, 800.f},
+        //
+        // // 110 octant 6 x ∈ [-1000, 0) y ∈ [0, 1000] z ∈ [0, 1000]
+        // {-50.f, 50.f, 50.f},
+        // {-300.f, 300.f, 300.f},
+        // {-550.f, 550.f, 550.f},
+        // {-800.f, 800.f, 800.f},
+        //
+        // // 111 octant 7 x ∈ [0, 1000] y ∈ [0, 1000] z ∈ [0, 1000]
+        // {50.f, 50.f, 50.f},
+        // {300.f, 300.f, 300.f},
+        // {550.f, 550.f, 550.f},
+        // {800.f, 800.f, 800.f},
+    };
+
+    //size_t n = 1000;
+    //std::vector<Particle> particles = generateParticles(n);
 
     auto bounds = findMinMax(particles);
     computeMortonCodes(particles, bounds);
@@ -196,12 +289,20 @@ int main() {
             break;
         }
     }
-    std::cout << "Monotonic Z_CODE: " << (monotone ? "OK" : "FAIL") << "\n\n\n";
 
     for (auto &p : particles) {
-        std::cout << p.Z_CODE << "\n";
+        //std::cout << p.Z_CODE << "\n";
+       // std::cout << std::bitset<64>(p.Z_CODE) << "\n";
+        std::cout << "(" << p.x << ", " << p.y << ", " << p.z << ")    ";
+        std::bitset<64> bits(p.Z_CODE);
+        for (int i = 63; i >= 0; --i)
+        {
+            std::cout << bits[i];
+            if (i == 63 || (i % 3 == 0 && i != 0))std::cout << ' ';
+        }
+        std::cout << '\n';
     }
-
+    std::cout << "Monotonic Z_CODE: " << (monotone ? "OK" : "FAIL") << "\n\n\n";
 
     //cudaApiTest();
     //glTest();
